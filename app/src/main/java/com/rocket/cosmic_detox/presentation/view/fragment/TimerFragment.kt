ad4097bdb73f9e5be88c9f22b16a4b0b0bd1d444
kkevi <bora_kim980@naver.com>
2024-08-27 14:52:13 +0900

    chore : 뒤로가기 버튼 클릭 시 Dialog 노출되도록 변경

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/TimerFragment.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/TimerFragment.kt
@@ -1,12 +1,15 @@
 package com.rocket.cosmic_detox.presentation.view.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.rocket.cosmic_detox.R
 import com.rocket.cosmic_detox.databinding.FragmentTimerBinding
+import com.rocket.cosmic_detox.presentation.component.dialog.OneButtonDialogFragment
 import com.rocket.cosmic_detox.presentation.component.dialog.TwoButtonDialogFragment
 
 class TimerFragment : Fragment() {
@@ -29,6 +32,17 @@ class TimerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        requireActivity().onBackPressedDispatcher.addCallback(requireActivity(), backPressedCallBack)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showTwoButtonDialog()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun initView() = with(binding) {
@@ -41,12 +55,24 @@ class TimerFragment : Fragment() {
             // 알림창이 띄워져있는 동안 배경 클릭 막기
             dialog.isCancelable = false
             dialog.show(getParentFragmentManager(), "ConfirmDialog")
-
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private val backPressedCallBack = object : OnBackPressedCallback(true) {
+        @SuppressLint("NotifyDataSetChanged")
+        override fun handleOnBackPressed() {
+            showTwoButtonDialog()
+        }
+    }
+
+    private fun showTwoButtonDialog(){
+        val dialog = OneButtonDialogFragment(
+            getString(R.string.dialog_common_focus)
+        ){
+            // 화면 강제 유지하는 코드
+        }
+        // 알림창이 띄워져있는 동안 배경 클릭 막기
+        dialog.isCancelable = false
+        dialog.show(getParentFragmentManager(), "ConfirmDialog")
     }
 }
