8c250952a33e0956456536aee4c593d61b9cd9c3
seojw0124 <wjddn2000124@gmail.com>
2024-08-28 19:07:13 +0900

    chore: 불필요한 코드 삭제

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/HomeFragment.kt
@@ -25,62 +25,6 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
-//        // 이미지뷰 받아오기 및 이렇게 가능한 코드인지 여쭤보기
-//
-//        val imageContainer = view?.findViewById<ConstraintLayout>(R.id.cl_home_illustration)
-//
-//        val imageResIds = listOf(
-//            R.drawable.mercury,
-//            R.drawable.mars,
-//            R.drawable.venus,
-//            R.drawable.earth,
-//            R.drawable.neptune,
-//            R.drawable.uranus,
-//            R.drawable.saturn,
-//            R.drawable.jupiter,
-//            R.drawable.sun
-//        )
-//
-//        for (i in imageResIds.indices) {
-//            val imageView = ImageView(requireContext())
-//
-//            imageView.setImageResource(imageResIds[i])
-//
-//            val imageSize = ConstraintLayout.LayoutParams(
-//                when (i) {
-//                    0 -> 100.dp
-//                    1 -> 120.dp
-//                    2 -> 140.dp
-//                    3 -> 145.dp
-//                    4 -> 160.dp
-//                    5 -> 222.dp
-//                    6 -> 265.dp
-//                    7 -> 290.dp
-//                    8 -> 345.dp
-//                },
-//                when (i) {
-//                    0 -> 100.dp
-//                    1 -> 120.dp
-//                    2 -> 140.dp
-//                    3 -> 145.dp
-//                    4 -> 160.dp
-//                    5 -> 222.dp
-//                    6 -> 265.dp
-//                    7 -> 290.dp
-//                    8 -> 345.dp
-//                }
-//            )
-//
-//            // margin 설정
-//            imageSize.setMargins(16.dp)
-//            imageView.layoutParams = imageSize
-//
-//            // ImageView cl에 추가
-//            imageContainer?.addView(imageView)
-//        }
-//
-//        return view
     }
 
     private val Int.dp: Int
