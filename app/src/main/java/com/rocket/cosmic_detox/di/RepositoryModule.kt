3d7e42a86533e3d87c3280ff21fc87e96496b1e3
ImGaram <gim78014@gmail.com>
2024-08-28 20:09:32 +0900

    feat: hilt module 생성

---
+++ app/src/main/java/com/rocket/cosmic_detox/di/RepositoryModule.kt
@@ -0,0 +1,15 @@
+package com.rocket.cosmic_detox.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+//    @Binds
+//    fun repository(
+//        repositoryImpl: RepositoryImpl
+//    ): Repository
+}
