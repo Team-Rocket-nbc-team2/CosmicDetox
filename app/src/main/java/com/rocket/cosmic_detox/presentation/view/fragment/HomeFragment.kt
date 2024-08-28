bde5ca603edd2dcc26dcaa9f57a5824950e94c09
seojw0124 <wjddn2000124@gmail.com>
2024-08-28 18:25:05 +0900

    feat: 홈에서 내 행성 이미지 로드하는 로직, 확장함수 추가

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/HomeFragment.kt
@@ -13,6 +13,10 @@ import androidx.navigation.fragment.findNavController
 import com.rocket.cosmic_detox.R
 import com.rocket.cosmic_detox.databinding.FragmentHomeBinding
 import com.rocket.cosmic_detox.presentation.component.dialog.TwoButtonDialogFragment
+import com.rocket.cosmic_detox.presentation.extensions.loadHomePlanetImage
+import com.rocket.cosmic_detox.presentation.extensions.loadRankingImage
+import com.rocket.cosmic_detox.presentation.model.RankingManager
+import java.math.BigDecimal
 
 class HomeFragment : Fragment() {
 
@@ -23,65 +27,65 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
 
-        // 이미지뷰 받아오기 및 이렇게 가능한 코드인지 여쭤보기
-
-        val imageContainer = view?.findViewById<ConstraintLayout>(R.id.cl_home_illustration)
-
-        val imageResIds = listOf(
-            R.drawable.mercury,
-            R.drawable.mars,
-            R.drawable.venus,
-            R.drawable.earth,
-            R.drawable.neptune,
-            R.drawable.uranus,
-            R.drawable.saturn,
-            R.drawable.jupiter,
-            R.drawable.sun
-        )
-
-        for (i in imageResIds.indices) {
-            val imageView = ImageView(requireContext())
-
-            imageView.setImageResource(imageResIds[i])
-
-            val imageSize = ConstraintLayout.LayoutParams(
-                when (i) {
-                    0 -> 100.dp
-                    1 -> 120.dp
-                    2 -> 140.dp
-                    3 -> 145.dp
-                    4 -> 160.dp
-                    5 -> 222.dp
-                    6 -> 265.dp
-                    7 -> 290.dp
-                    8 -> 345.dp
-                },
-                when (i) {
-                    0 -> 100.dp
-                    1 -> 120.dp
-                    2 -> 140.dp
-                    3 -> 145.dp
-                    4 -> 160.dp
-                    5 -> 222.dp
-                    6 -> 265.dp
-                    7 -> 290.dp
-                    8 -> 345.dp
-                }
-            )
-
-            // margin 설정
-            imageSize.setMargins(16.dp)
-            imageView.layoutParams = imageSize
-
-            // ImageView cl에 추가
-            imageContainer?.addView(imageView)
-        }
-
-        return view
+//        // 이미지뷰 받아오기 및 이렇게 가능한 코드인지 여쭤보기
+//
+//        val imageContainer = view?.findViewById<ConstraintLayout>(R.id.cl_home_illustration)
+//
+//        val imageResIds = listOf(
+//            R.drawable.mercury,
+//            R.drawable.mars,
+//            R.drawable.venus,
+//            R.drawable.earth,
+//            R.drawable.neptune,
+//            R.drawable.uranus,
+//            R.drawable.saturn,
+//            R.drawable.jupiter,
+//            R.drawable.sun
+//        )
+//
+//        for (i in imageResIds.indices) {
+//            val imageView = ImageView(requireContext())
+//
+//            imageView.setImageResource(imageResIds[i])
+//
+//            val imageSize = ConstraintLayout.LayoutParams(
+//                when (i) {
+//                    0 -> 100.dp
+//                    1 -> 120.dp
+//                    2 -> 140.dp
+//                    3 -> 145.dp
+//                    4 -> 160.dp
+//                    5 -> 222.dp
+//                    6 -> 265.dp
+//                    7 -> 290.dp
+//                    8 -> 345.dp
+//                },
+//                when (i) {
+//                    0 -> 100.dp
+//                    1 -> 120.dp
+//                    2 -> 140.dp
+//                    3 -> 145.dp
+//                    4 -> 160.dp
+//                    5 -> 222.dp
+//                    6 -> 265.dp
+//                    7 -> 290.dp
+//                    8 -> 345.dp
+//                }
+//            )
+//
+//            // margin 설정
+//            imageSize.setMargins(16.dp)
+//            imageView.layoutParams = imageSize
+//
+//            // ImageView cl에 추가
+//            imageContainer?.addView(imageView)
+//        }
+//
+//        return view
     }
 
     private val Int.dp: Int
@@ -107,6 +111,7 @@ class HomeFragment : Fragment() {
             dialog.isCancelable = false
             dialog.show(getParentFragmentManager(), "ConfirmDialog")
         }
+        binding.ivHomeMyPlanet.loadHomePlanetImage(BigDecimal(800000))
     }
 
     override fun onDestroyView() {
