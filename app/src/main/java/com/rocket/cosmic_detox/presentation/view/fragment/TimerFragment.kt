1250dd40bc76f523fac998430c0da508cc887d40
chanhue <chanhue467@gmail.com>
2024-08-29 15:35:52 +0900

    fix : oncreate() 메서드 제거

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/TimerFragment.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/TimerFragment.kt
@@ -23,9 +23,7 @@ class TimerFragment : Fragment() {
     private var _binding: FragmentTimerBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
