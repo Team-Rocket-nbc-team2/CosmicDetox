dd3f3c7d411d8b325c80ebd06d71e8d493db2774
ImGaram <gim78014@gmail.com>
2024-08-30 15:45:53 +0900

    feat: repository hilt module에 allowAppRepository 추가

--- app/src/main/java/com/rocket/cosmic_detox/di/RepositoryModule.kt
+++ app/src/main/java/com/rocket/cosmic_detox/di/RepositoryModule.kt
@@ -1,15 +1,16 @@
 package com.rocket.cosmic_detox.di
 
+import com.rocket.cosmic_detox.data.repository.AllowedAppRepositoryImpl
+import com.rocket.cosmic_detox.domain.repository.AllowedAppRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-//    @Binds
-//    fun repository(
-//        repositoryImpl: RepositoryImpl
-//    ): Repository
+    @Binds
+    abstract fun bindAllowAppRepository(allowedAppRepositoryImpl: AllowedAppRepositoryImpl): AllowedAppRepository
 }
