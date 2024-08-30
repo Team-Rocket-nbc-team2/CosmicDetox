432470886dd922d5c5da902afe30a3b828bb4332
kkevi <bora_kim980@naver.com>
2024-08-30 19:53:26 +0900

    feat : 최초 로그인 / 재로그인 구분하여 로직 변경

--- app/src/main/java/com/rocket/cosmic_detox/domain/repository/SignInRepository.kt
+++ app/src/main/java/com/rocket/cosmic_detox/domain/repository/SignInRepository.kt
@@ -1,7 +1,15 @@
 package com.rocket.cosmic_detox.domain.repository
 
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.rocket.cosmic_detox.data.model.App
+import com.rocket.cosmic_detox.presentation.view.fragment.mypage.adapter.Trophy
+
 interface SignInRepository {
-    fun googleLogin()
+    fun setDataToFireBase(auth: FirebaseAuth) {
+    }
+
+    fun googleLogin(idToken: String)
 
     fun kakaoLogin()
 
