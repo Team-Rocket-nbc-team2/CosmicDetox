87baf2464e920acdf868e1f4fd7b2a1993842930
chanhue <chanhue467@gmail.com>
2024-08-31 23:25:55 +0900

    feat : 앱 모듈에 UserRepository를 위한 Hilt 프로바이더 추가

--- app/src/main/java/com/rocket/cosmic_detox/di/AppModule.kt
+++ app/src/main/java/com/rocket/cosmic_detox/di/AppModule.kt
@@ -2,11 +2,17 @@ package com.rocket.cosmic_detox.di
 
 import android.content.Context
 import android.content.pm.PackageManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.rocket.cosmic_detox.data.repository.UserRepositoryImpl
+import com.rocket.cosmic_detox.domain.repository.UserRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,4 +22,15 @@ object AppModule {
     fun providePackageManager(@ApplicationContext context: Context): PackageManager {
         return context.packageManager
     }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(
+        firestore: FirebaseFirestore,
+        auth: FirebaseAuth
+    ): UserRepository {
+        return UserRepositoryImpl(firestore, auth)
+    }
+
+
 }
