6b0ae1557f004de3369638ab20ef6a7a07965caf
chanhue <chanhue467@gmail.com>
2024-08-31 23:30:26 +0900

    fix : usecase , ui state , ViewModel 주입

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/timer/TimerFragment.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/timer/TimerFragment.kt
@@ -7,15 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.rocket.cosmic_detox.R
 import com.rocket.cosmic_detox.databinding.FragmentTimerBinding
 import com.rocket.cosmic_detox.presentation.component.bottomsheet.TimerAllowedAppBottomSheet
 import com.rocket.cosmic_detox.presentation.component.dialog.OneButtonDialogFragment
 import com.rocket.cosmic_detox.presentation.component.dialog.TwoButtonDialogFragment
+import com.rocket.cosmic_detox.presentation.uistate.UiState
+import com.rocket.cosmic_detox.presentation.view.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import android.os.Handler
 import android.os.Looper
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TimerFragment : Fragment() {
@@ -26,6 +31,9 @@ class TimerFragment : Fragment() {
     private var time = 0 // 시간
     private val handler = Handler(Looper.getMainLooper()) // 메인 스레드에서 실행할 핸들러
     private var isTimerRunning = false // 타이머가 실행 중인지 확인하는 변수
+
+    private val userViewModel: UserViewModel by viewModels()
+
     private val runnable = object : Runnable {
         override fun run() {
             updateTime()
@@ -47,8 +55,46 @@ class TimerFragment : Fragment() {
         initView()
         requireActivity().onBackPressedDispatcher.addCallback(requireActivity(), backPressedCallBack)
 
+        observeViewModel()
+
+        userViewModel.fetchTotalTime()  // ViewModel을 통해 totalTime을 불러오기
         startTimer()  // 뷰가 생성될 때 타이머 바로 시작
     }
+
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            userViewModel.totalTimeState.collect { state ->
+                when (state) {
+                    is UiState.Loading -> {
+                        // 추후 작업필요할 떄 활용 로딩 상태 처리 (예: ProgressBar 표시)
+                    }
+                    is UiState.Success -> {
+                        time = state.data.toInt()
+                        updateTime()
+                    }
+                    is UiState.Failure -> {
+                        showError(state.e?.message)
+                    }
+                    is UiState.Init -> {
+                        // 초기 상태 처리코드 추후 작성
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showError(message: String?) {
+        val dialog = OneButtonDialogFragment(
+            title = message ?: getString(R.string.dialog_common_error),
+            onClickConfirm = {
+                // 오류처리는 추후에 작성
+                // ex "확인" 버튼 클릭 시 실행할 작업 여기에 작성하면됨
+            }
+        )
+        dialog.isCancelable = false
+        dialog.show(parentFragmentManager, "ErrorDialog")
+    }
+
 
     override fun onPause() {
         super.onPause()
@@ -68,7 +114,7 @@ class TimerFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        stopTimer()  // 뷰가 파괴될 때 타이머를 중지합니다.
+        stopTimer()  // 뷰가 파괴될 때 타이머를 중지
     }
 
     private fun initView() = with(binding) {
@@ -117,6 +163,9 @@ class TimerFragment : Fragment() {
     private fun stopTimer() {
         handler.removeCallbacks(runnable)
         isTimerRunning = false  // 타이머 실행 상태를 false로 설정
+
+        // 타이머가 중지될 때 totalTime을 Firestore에 저장
+        userViewModel.updateTotalTime(time.toLong())
     }
 
     private fun updateTime() {
