e01af9fe455bb06ad26db762cb1ff979ccde920f
kkevi <bora_kim980@naver.com>
2024-09-03 21:25:33 +0900

    remove : 불필요한 변수 삭제

--- app/src/main/java/com/rocket/cosmic_detox/data/repository/SignInRepositoryImpl.kt
+++ app/src/main/java/com/rocket/cosmic_detox/data/repository/SignInRepositoryImpl.kt
@@ -3,8 +3,6 @@ package com.rocket.cosmic_detox.data.repository
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.rocket.cosmic_detox.data.model.AllowedApp
-import com.rocket.cosmic_detox.data.model.Trophy
 import com.rocket.cosmic_detox.domain.repository.SignInRepository
 import javax.inject.Inject
 
@@ -33,10 +31,7 @@ class SignInRepositoryImpl @Inject constructor(
                 }
             } else {
                 // 최초 로그인 (회원가입)
-                val trophies = listOf<Trophy>()
-                val apps = listOf<AllowedApp>()
 
-                // TODO : User DataClass 생성 되면 DataClass 맞춰서 생성할 것
                 val firstUser = hashMapOf(
                     "uID" to uId,
                     "name" to authUser?.displayName.toString(),
@@ -56,9 +51,15 @@ class SignInRepositoryImpl @Inject constructor(
                     "totalTime" to 0,
                 )
 
+<<<<<<< Updated upstream
                 rankingUserRef.collection("ranking").add(firstRankingUser)
                     .addOnSuccessListener { Log.d("User Data의 Apps 전송 성공", "App document written!") }
                     .addOnFailureListener { exception -> Log.w("Firestore", "Error adding app document", exception) }
+=======
+                rankingUserRef.collection("ranking").document(uId).set(firstRankingUser)
+                    .addOnSuccessListener { Log.d("Ranking User Data 전송 성공", "App document written!") }
+                    .addOnFailureListener { exception -> Log.w("Ranking User Data 전송 실패", "Error adding app document", exception) }
+>>>>>>> Stashed changes
 
                 userRef.set(firstUser)
                     .addOnSuccessListener { Log.d("User Data 전송 성공", "User data is successfully written!") }
