ca41fd7657aff990392c213a93f6de2a26ee9c90
ImGaram <gim78014@gmail.com>
2024-09-03 17:00:12 +0900

    feat: 현재 사용 중인 앱 패키지 불러오기, 선택된 앱의 남은 제한 시간 업데이트 함수 구현

--- app/src/main/java/com/rocket/cosmic_detox/presentation/viewmodel/AllowedAppViewModel.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/viewmodel/AllowedAppViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rocket.cosmic_detox.data.model.AllowedApp
 import com.rocket.cosmic_detox.domain.usecase.timer.GetAllowedAppUseCase
+import com.rocket.cosmic_detox.domain.usecase.timer.UpdateLimitedTimeAppUseCase
 import com.rocket.cosmic_detox.presentation.uistate.GetListUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,13 +14,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AllowedAppViewModel @Inject constructor(
-    private val getAllowedAppUseCase: GetAllowedAppUseCase
+    private val getAllowedAppUseCase: GetAllowedAppUseCase,
+    private val updateLimitedTimeAppUseCase: UpdateLimitedTimeAppUseCase
 ): ViewModel() {
     private val _allowedAppList = MutableStateFlow<GetListUiState<List<AllowedApp>>>(GetListUiState.Init)
     val allowedAppList: StateFlow<GetListUiState<List<AllowedApp>>> = _allowedAppList
 
-    private val _countDownRemainTime = MutableStateFlow<Long?>(null)
-    val countDownRemainTime: StateFlow<Long?> = _countDownRemainTime
+    private val _countDownRemainTime = MutableStateFlow<Int?>(null)
+    val countDownRemainTime: StateFlow<Int?> = _countDownRemainTime
+
+    private val _selectedAllowedAppPackage = MutableStateFlow<String?>(null)
+    val selectedAllowedAppPackage: StateFlow<String?> = _selectedAllowedAppPackage
 
     fun getAllAllowedApps() {
         viewModelScope.launch {
@@ -37,7 +42,25 @@ class AllowedAppViewModel @Inject constructor(
         }
     }
 
-    fun updateRemainTime(remainTime: Long) {
+    fun setSelectedAllowedAppPackage(packageId: String) {
+        _selectedAllowedAppPackage.value = packageId
+    }
+
+    fun updateRemainTime(remainTime: Int) {
         _countDownRemainTime.value = remainTime
+    }
+
+    fun updateLimitedTimeAllowApp(
+        packageId: String,
+        remainTime: Int,
+        failCallback: (Throwable?) -> Unit
+    ) {
+        viewModelScope.launch {
+            updateLimitedTimeAppUseCase(
+                packageId = packageId,
+                remainTime = remainTime,
+                failCallback = failCallback
+            )
+        }
     }
 }
