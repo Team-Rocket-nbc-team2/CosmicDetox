4744b680ab9d02fd0ca4c22c30b5eadcc52c6b08
ggilggilmonster <god01133@naver.com>
2024-09-06 15:14:34 +0900

    feat : DialogActivity에서 ViewCompat 적용 TODO : 상단바는 적용이 안되는 문제(열품타도 동일)

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/activity/DialogActivity.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/activity/DialogActivity.kt
@@ -2,6 +2,9 @@ package com.rocket.cosmic_detox.presentation.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rocket.cosmic_detox.R
 import com.rocket.cosmic_detox.databinding.ActivityDialogBinding
 
 class DialogActivity : AppCompatActivity() {
@@ -13,6 +16,11 @@ class DialogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
 
         binding.btnBack.setOnClickListener {
             finish()
