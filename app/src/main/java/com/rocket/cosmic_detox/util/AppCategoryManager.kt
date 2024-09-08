cd892f629bbc6332d87def2b42b9012e95daa8e3
seojw0124 <wjddn2000124@gmail.com>
2024-09-08 16:00:09 +0900

    chore: 앱 사용시간 제한 디폴트 값 수정

--- app/src/main/java/com/rocket/cosmic_detox/util/AppCategoryManager.kt
+++ app/src/main/java/com/rocket/cosmic_detox/util/AppCategoryManager.kt
@@ -8,13 +8,13 @@ object AppCategoryManager {
     fun getLimitedTimeByCategory(category: Int): Long {
         return when(category) {
             ApplicationInfo.CATEGORY_GAME -> 30L.fromMinutesToSeconds()
-            ApplicationInfo.CATEGORY_AUDIO -> 60L.fromMinutesToSeconds()
-            ApplicationInfo.CATEGORY_VIDEO -> 60L.fromMinutesToSeconds()
+            ApplicationInfo.CATEGORY_AUDIO -> 30L.fromMinutesToSeconds()
+            ApplicationInfo.CATEGORY_VIDEO -> 30L.fromMinutesToSeconds()
             ApplicationInfo.CATEGORY_IMAGE -> 30L.fromMinutesToSeconds()
-            ApplicationInfo.CATEGORY_SOCIAL -> 60L.fromMinutesToSeconds()
-            ApplicationInfo.CATEGORY_NEWS -> 60L.fromMinutesToSeconds()
+            ApplicationInfo.CATEGORY_SOCIAL -> 30L.fromMinutesToSeconds()
+            ApplicationInfo.CATEGORY_NEWS -> 30L.fromMinutesToSeconds()
             ApplicationInfo.CATEGORY_MAPS -> 30L.fromMinutesToSeconds()
-            ApplicationInfo.CATEGORY_PRODUCTIVITY -> 60L.fromMinutesToSeconds()
+            ApplicationInfo.CATEGORY_PRODUCTIVITY -> 30L.fromMinutesToSeconds()
             else -> 30L.fromMinutesToSeconds()
         }
     }
