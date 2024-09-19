823ca4eff1ca6fc67a0ed6f7b7ee0804caa60878
chanhue <chanhue467@gmail.com>
2024-09-19 16:55:11 +0900

    feat : 정확한 시간에 알림이 울리도록 수정 및 타이머 시간이 시간 단위 이상이면 totalTime에 반영되는 오류 해결

--- app/src/main/java/com/rocket/cosmic_detox/presentation/receiver/MidnightResetReceiver.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/receiver/MidnightResetReceiver.kt
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.rocket.cosmic_detox.presentation.CosmicDetoxApplication
 import com.rocket.cosmic_detox.presentation.service.TimerService
 
 class MidnightResetReceiver : BroadcastReceiver() {
@@ -17,6 +18,14 @@ class MidnightResetReceiver : BroadcastReceiver() {
                 action = TimerService.ACTION_RESET_TIMER
             }
             context.startService(resetIntent)  // TimerService에 초기화 요청
+
+/*
+            // 임시 주석 , 자정 알림하려면 풀기
+            // 다음 자정을 위한 알람 재설정
+            val app = context.applicationContext as CosmicDetoxApplication
+            app.scheduleExactAlarm(context)  // 다음 자정을 위해 다시 알람 설정
+
+ */
         }
     }
 }
