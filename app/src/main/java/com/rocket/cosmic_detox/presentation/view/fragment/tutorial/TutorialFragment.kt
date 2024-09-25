70dfeb3b4fcf85906762c4dee9fbd511647b9b4e
ggilggilmonster <god01133@naver.com>
2024-09-25 21:14:35 +0900

    refactor : viewBinding 및 중복 코드 함수로 처리

--- app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/tutorial/TutorialFragment.kt
+++ app/src/main/java/com/rocket/cosmic_detox/presentation/view/fragment/tutorial/TutorialFragment.kt
@@ -1,6 +1,5 @@
 package com.rocket.cosmic_detox.presentation.view.fragment.tutorial
 
-import android.annotation.SuppressLint
 import android.graphics.Typeface
 import android.os.Bundle
 import android.util.TypedValue
@@ -8,71 +7,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.core.view.marginBottom
-import androidx.core.view.marginTop
+import androidx.core.view.isVisible
 import androidx.core.view.setPadding
 import com.rocket.cosmic_detox.R
+import com.rocket.cosmic_detox.databinding.FragmentTutorialBinding
 import com.rocket.cosmic_detox.presentation.view.activity.TutorialActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TutorialFragment : Fragment() {
 
+    private var _binding: FragmentTutorialBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_tutorial, container, false)
+        _binding = FragmentTutorialBinding.inflate(inflater, container, false)
 
         val position = arguments?.getInt("position") ?: 0
-        val imageView = view.findViewById<ImageView>(R.id.iv_tutorial_icon)
-        val textView1 = view.findViewById<TextView>(R.id.tv_tutorial_addiction)
-        val textView2 = view.findViewById<TextView>(R.id.tv_tutorial_cosmic)
-        val button = view.findViewById<Button>(R.id.btn_tutorial)
 
-        when (position) {
-            0 -> {
-                imageView.setImageResource(R.drawable.tutorial_icon)
-                textView1.text = getString(R.string.tutorial_addiction)
-                textView2.text = getString(R.string.tutorial_cosmic)
-            }
-            1 -> {
-                imageView.setImageResource(R.drawable.tutorial_allowed_app)
-                textView2.text = getString(R.string.tutorial_app)
-                textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-                textView2.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-                textView2.setTypeface(null, Typeface.BOLD)
-            }
-            2 -> {
-                imageView.setImageResource(R.drawable.tutorial_overlay)
-                textView2.text = getString(R.string.tutorial_overlay)
-                textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-                textView2.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-                textView2.setTypeface(null, Typeface.BOLD)
-            }
-            3 -> {
-                imageView.setImageResource(R.drawable.tutorial_race)
-                textView2.text = getString(R.string.tutorial_race)
-                textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-                textView2.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-                textView2.setTypeface(null, Typeface.BOLD)
-            }
-            4 -> {
-                imageView.setImageResource(R.drawable.tutorial_planet)
-                imageView.setPadding(15)
-                textView2.text = getString(R.string.tutorial_planet)
-                textView2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-                textView2.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-                textView2.setTypeface(null, Typeface.BOLD)
-                button.text = getString(R.string.tutorial_start)
-            }
-        }
+        // position에 따라 이미지, 텍스트 및 버튼 설정
+        setupTutorialPage(position)
 
-        button.setOnClickListener {
+        // 버튼 클릭 리스너 설정
+        binding.btnTutorial.setOnClickListener {
             if (position < 4) {
                 // 다음 페이지로 이동
                 (activity as? TutorialActivity)?.moveToNextPage(position)
@@ -82,7 +43,100 @@ class TutorialFragment : Fragment() {
             }
         }
 
-        return view
+        return binding.root
+    }
+
+    // 프래그먼트의 각 페이지에 대한 설정을 처리하는 함수
+    private fun setupTutorialPage(position: Int) {
+        when (position) {
+            0 -> {
+                setPageContent(
+                    R.drawable.tutorial_icon,
+                    R.string.tutorial_addiction,
+                    R.string.tutorial_cosmic,
+                    R.string.tutorial_next
+                )
+            }
+            1 -> {
+                setPageContent(
+                    R.drawable.tutorial_allowed_app,
+                    null,
+                    R.string.tutorial_app,
+                    R.string.tutorial_next,
+                    20f,
+                    R.color.white,
+                    Typeface.BOLD
+                )
+            }
+            2 -> {
+                setPageContent(
+                    R.drawable.tutorial_overlay,
+                    null,
+                    R.string.tutorial_overlay,
+                    R.string.tutorial_next,
+                    20f,
+                    R.color.white,
+                    Typeface.BOLD
+                )
+            }
+            3 -> {
+                setPageContent(
+                    R.drawable.tutorial_race,
+                    null,
+                    R.string.tutorial_race,
+                    R.string.tutorial_next,
+                    20f,
+                    R.color.white,
+                    Typeface.BOLD
+                )
+            }
+            4 -> {
+                setPageContent(
+                    R.drawable.tutorial_planet,
+                    null,
+                    R.string.tutorial_planet,
+                    R.string.tutorial_start,
+                    20f,
+                    R.color.white,
+                    Typeface.BOLD
+                )
+                binding.ivTutorialIcon.setPadding(15)
+            }
+        }
+    }
+
+    // 이미지 및 텍스트 설정을 위한 함수
+    private fun setPageContent(
+        imageResId: Int,
+        text1ResId: Int? = null,
+        text2ResId: Int,
+        buttonTextResId: Int,
+        textSize: Float = 14f,
+        textColorResId: Int = R.color.white20,
+        textStyle: Int = Typeface.NORMAL
+    ) {
+        binding.ivTutorialIcon.setImageResource(imageResId)
+
+        text1ResId?.let {
+            binding.tvTutorialAddiction.text = getString(it)
+            binding.tvTutorialAddiction.isVisible = true
+        } ?: run {
+            binding.tvTutorialAddiction.isVisible = false
+        }
+
+        binding.tvTutorialCosmic.apply {
+            text = getString(text2ResId)
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
+            setTextColor(ContextCompat.getColor(requireContext(), textColorResId))
+            setTypeface(null, textStyle)
+        }
+
+        binding.btnTutorial.text = getString(buttonTextResId)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
