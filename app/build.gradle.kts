aa2a6e415b5214b06a6b965c5d96b64d1d07f6da
ggilggilmonster <god01133@naver.com>
2024-09-30 15:35:19 +0900

    release : versionName 1.1.0 versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.rocket.cosmic_detox"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
