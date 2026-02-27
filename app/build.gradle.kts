926e78ee647a3f0db6a3d03d0f4306f6396f4ce7
seojw0124 <wjddn2000124@gmail.com>
2026-02-28 05:36:59 +0900

    build: local.properties 따옴표 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "KAKAO_APP_KEY", properties.getProperty("KAKAO_APP_KEY"))
-        resValue("string", "KAKAO_OAUTH_HOST", properties.getProperty("KAKAO_OAUTH_HOST"))
+        buildConfigField("String", "KAKAO_APP_KEY", "\"${properties["KAKAO_APP_KEY"]}\"")
+        resValue("string", "KAKAO_OAUTH_HOST", "\"${properties["KAKAO_OAUTH_HOST"]}\"")
     }
 
     signingConfigs {
