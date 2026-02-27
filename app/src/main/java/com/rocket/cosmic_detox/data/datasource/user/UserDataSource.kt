61e890e92f95521de9c55ca32382e3c143fba84d
seojw0124 <wjddn2000124@gmail.com>
2026-02-28 07:07:53 +0900

    chore: CI 실패 테스트를 위한 코드2 복구

--- app/src/main/java/com/rocket/cosmic_detox/data/datasource/user/UserDataSource.kt
+++ app/src/main/java/com/rocket/cosmic_detox/data/datasource/user/UserDataSource.kt
@@ -7,7 +7,7 @@ import java.util.Date
 
 interface UserDataSource {
 
-    suspend fun getUid(): Stringㅁㅁㅁ
+    suspend fun getUid(): String
 
     suspend fun getUserCreatedDate(uid: String): Result<Date>
 
