f1b587fb732e548a5978afa8dbc15f2784e55a2e
seojw0124 <wjddn2000124@gmail.com>
2026-03-21 15:07:00 +0900

    build: Gradle, 라이브러리 버전 업데이트

--- app/src/main/java/com/rocket/cosmic_detox/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/rocket/cosmic_detox/data/repository/UserRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.rocket.cosmic_detox.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.firestore.toObject
 import com.rocket.cosmic_detox.data.model.User
 import com.rocket.cosmic_detox.domain.repository.UserRepository
 import javax.inject.Inject
