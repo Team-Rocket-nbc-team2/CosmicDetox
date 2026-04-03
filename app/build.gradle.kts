f210631dfaca5375567d47a4de3c134936a4d622
seojw0124 <wjddn2000124@gmail.com>
2026-04-03 21:19:09 +0900

    build: versionCode 6으로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.rocket.cosmic_detox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
