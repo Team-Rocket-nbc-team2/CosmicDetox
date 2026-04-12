0f398b18bd36ed01f491ee5671695e246dc9e4b6
seojw0124 <wjddn2000124@gmail.com>
2026-04-13 05:14:42 +0900

    build: versionCode = 8, versionName = 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.rocket.cosmic_detox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.1"
+        versionCode = 8
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
